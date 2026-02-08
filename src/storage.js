10ca360fab76ae908143e3003ae4308d97a98423
Oliver-Marcus Eder <github@derkonzern.com>
2026-02-08 05:39:21 +0100

    Update storage.js

--- src/storage.js
+++ src/storage.js
@@ -6,10 +6,6 @@ if (!fs.existsSync(ICS_DIR)) fs.mkdirSync(ICS_DIR, { recursive: true });
 
 function saveICS(teamId, type, data) {
   const filepath = path.join(ICS_DIR, `${teamId}_${type}.ics`);
-  // Explizit UTF-8 mit BOM für bessere Kompatibilität
-  const utf8BOM = '\uFEFF';
-  fs.writeFileSync(filepath, utf8BOM + data, { encoding: 'utf8' });
-  console.log(`[STORAGE] Datei geschrieben: ${filepath} (${data.length} Bytes)`);
   return filepath;
 }
 
