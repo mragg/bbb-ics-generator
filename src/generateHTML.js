6a93b22dd22027203ab127e9c79ce9fc7c5a4323
celloschach <raggelija@gmail.com>
2026-02-10 21:32:25 +0100

    Fix ageGroup display in generateHTML.js

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -98,7 +98,7 @@ document.querySelectorAll('.step-header').forEach(header => {
 
   ${teams.map(t => `
     <div class="team">
-      <strong>${t.teamName}</strong> (${t.ageGroup})<br/>
+     <strong>${t.teamName}</strong>${t.ageGroup ? ` (${t.ageGroup})` : ''}<br/>
       <small>Letztes Update: ${new Date(t.lastUpdate).toLocaleString('de-DE')}</small><br/>
       ${t.matchCount} Spiele, Heim: ${t.homeMatchCount}, Auswärts: ${t.awayMatchCount}<br/>
       <div class="buttons">
